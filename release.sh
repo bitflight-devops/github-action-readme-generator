4d0d7b30859b6d4edce3a216feb2a73f95115b8d
jamie-wearsafe <jamie@wearsafe.com>
2022-08-10 09:45:46 -0400

    feat(bin): update for release

--- release.sh
+++ release.sh
@@ -5,27 +5,22 @@ bump="${1:-patch}"
 newtag="$(git semver "${bump}" --dryrun)"
 yarntag="$(jq -r '.version' package.json)"
 if [[ ${yarntag} != "${newtag#v}" ]]; then
-yarn version -i "${newtag}" || true
-yarn build
+yarn version -i "${newtag#v}" || true
 fi
-
+yarn build
 git add dist package.json yarn.lock .yarn
 git commit -m "chore(release): bump version to ${newtag}" --no-verify
 git semver "${bump}"
-newtag2="$(git semver get)"
-# if [[ "${newtag}" != "${newtag2}" ]]; then
-#   echo "ERROR: new tag does not match expected tag"
-#   echo "  expected: ${newtag}"
-#   echo "  actual:   ${newtag2}"
-#   exit 1
-# fi
 
+newtag2="$(git semver get)"
 stub_major="${newtag%%\.*}"
 stub_major_minor="${newtag%\.*}"
+git tag -d "${newtag2}" 2>/dev/null || true
 git tag -d "${stub_major}" 2>/dev/null || true
 git tag -d "${stub_major_minor}" 2>/dev/null || true
 git tag -a "${stub_major}" -m "Release ${newtag}"
 git tag -a "${stub_major_minor}" -m "Release ${newtag}"
+git push origin ":${newtag2}" 2>/dev/null || true
 git push origin ":${stub_major}" 2>/dev/null || true
 git push origin ":${stub_major_minor}" 2>/dev/null || true
 yarn postversion

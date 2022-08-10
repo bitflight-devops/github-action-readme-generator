a9d795506c8229c7ab357ae1d9aa9be531688e95
jamie-wearsafe <jamie@wearsafe.com>
2022-08-10 10:13:58 -0400

    feat(bin): update for release

--- release.sh
+++ release.sh
@@ -17,12 +17,12 @@ git semver "${bump}"
 newtag2="$(git semver get)"
 stub_major="${newtag%%\.*}"
 stub_major_minor="${newtag%\.*}"
-git tag -d "${newtag2}" 2>/dev/null || true
+
 git tag -d "${stub_major}" 2>/dev/null || true
 git tag -d "${stub_major_minor}" 2>/dev/null || true
 git tag -a "${stub_major}" -m "Release ${newtag}"
 git tag -a "${stub_major_minor}" -m "Release ${newtag}"
-git push origin ":${newtag2}" 2>/dev/null || true
+
 git push origin ":${stub_major}" 2>/dev/null || true
 git push origin ":${stub_major_minor}" 2>/dev/null || true
 yarn postversion

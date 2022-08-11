2e670c4ac0fd34669be977f96862b3dcb675e713
jamie-wearsafe <jamie@wearsafe.com>
2022-08-11 15:57:29 -0400

    feat(markdown): update table escape regex

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -124,6 +124,19 @@ declare module "src/sections/update-description" {
 declare module "src/markdowner/index" {
     export type MarkdownArrayRowType = string[][];
     export type MarkdownArrayItemType = string;
+    /**
+     * Fills the width of the cell.
+     * @param text
+     * @param width
+     * @param paddingStart
+     */
+    export function fillWidth(text: string, width: number, paddingStart: number): string;
+    /**
+     * Escape a text so it can be used in a markdown table
+     * @param text
+     */
+    export function markdownEscapeTableCell(text: string): string;
+    export function markdownEscapeInlineCode(content: string): string;
     export function ArrayOfArraysToMarkdownTable(providedTableContent: MarkdownArrayRowType): string;
     export default ArrayOfArraysToMarkdownTable;
 }

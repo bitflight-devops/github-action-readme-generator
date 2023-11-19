b83a5436e8993ed6e33e2937c213381e171164e2
jamie-bitflight <jamie@bitflight.io>
2023-11-19 13:32:27 -0500

    ci(coderabbit): add .coderabbit.yaml file for code review configuration

--- dist/mjs/util.d.ts
+++
@@ -1,2 +0,0 @@
-export type Nullable<T> = T | null | undefined;
-export declare function notEmpty(str: Nullable<string>): str is string;
--- dist/mjs/util.js
+++
@@ -1,4 +0,0 @@
-export function notEmpty(str) {
-    return typeof str === 'string' ? str.trim().length > 0 : false;
-}
-//# sourceMappingURL=util.js.map

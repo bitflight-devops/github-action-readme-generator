37687bfb90a1c9f3743c536adcde2954f16a951f
jamie-bitflight <jamie@bitflight.io>
2023-11-20 13:12:42 -0500

    ci(lint): fix false positive on markdown lint

--- vitest.config.ts
+++ vitest.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     },
     coverage: {
       provider: 'v8',
-      reportsDirectory: './coverage',
+      reportsDirectory: './out',
       reporter: ['text', 'json-summary', 'json'],
       include: ['src/'],
     },
